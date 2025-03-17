38225d7881c03719a211d272e372923f69899324
Ian Wafula <neocorpse@proton.me>
2025-03-17 13:45:54 +0300

    Create click event for like buttons

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
 
+const modal = document.querySelector('#modal');
+const message = modal.querySelector('p');
 
-
+document.querySelectorAll('.like-glyph').forEach((btn) => {
+	btn.addEventListener('click', () => {
+		mimicServerCall()
+			.then((res) => {
+				if (btn.classList.contains('activated-heart')) {
+					btn.textContent = EMPTY_HEART;
+				} else {
+					btn.textContent = FULL_HEART;
+				}
+				btn.classList.toggle('activated-heart');
+			})
+			.catch((error) => {
+				modal.classList.toggle('hidden');
+				message.innerHTML = `${error}`;
+				console.log(error);
+				setTimeout(() => {
+					modal.classList.toggle('hidden');
+				}, 3000);
+			});
+	});
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
-      if (isRandomFailure) {
-        reject("Random server error. Try again.");
-      } else {
-        resolve("Pretend remote server notified of action!");
-      }
-    }, 300);
-  });
+function mimicServerCall(url = 'http://mimicServer.example.com', config = {}) {
+	return new Promise(function (resolve, reject) {
+		setTimeout(function () {
+			let isRandomFailure = Math.random() < 0.2;
+			if (isRandomFailure) {
+				reject('Random server error. Try again.');
+			} else {
+				resolve('Pretend remote server notified of action!');
+			}
+		}, 300);
+	});
 }
